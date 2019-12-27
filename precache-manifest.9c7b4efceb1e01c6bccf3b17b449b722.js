a5e1d3700316a4123331cec11ea21fe4fb89c3ff
vgeroim <vgeroim.qa@gmail.com>
2019-12-27 17:37:11 +0000

    Updates

---
+++ precache-manifest.9c7b4efceb1e01c6bccf3b17b449b722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfe84c97449030d93b14887aaab12bb",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "bafe847c34ff9a962480",
+    "url": "/counter/static/css/main.f67291fc.chunk.css"
+  },
+  {
+    "revision": "d417d2c75097c1f3347e",
+    "url": "/counter/static/js/2.255a2edd.chunk.js"
+  },
+  {
+    "revision": "bafe847c34ff9a962480",
+    "url": "/counter/static/js/main.a6630d67.chunk.js"
+  },
+  {
+    "revision": "f1e7dee04ecec213fd5d",
+    "url": "/counter/static/js/runtime-main.25b4d636.js"
+  }
+]);
